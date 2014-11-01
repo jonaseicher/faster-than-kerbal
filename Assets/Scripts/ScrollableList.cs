d652de634dd1b26ae3fd5f1fb52056530e015c03
Jonas <Jonaseicher@hotmail.com>
2014-11-01 02:51:42 +0100

    ship scene created, scrollable list fixes

--- Assets/Scripts/ScrollableList.cs
+++ Assets/Scripts/ScrollableList.cs
@@ -9,29 +9,45 @@ public class ScrollableList : MonoBehaviour
     int columnCount = 1;
     GameWidgetManager gameWidget;
 
+    RectTransform rowRectTransform;
+    RectTransform containerRectTransform;
+
     void Awake()
     {
         gameWidget = itemPrefab.GetComponent<GameWidgetManager>();
+        rowRectTransform = itemPrefab.GetComponent<RectTransform>();
+        containerRectTransform = gameObject.GetComponent<RectTransform>();
         
     }
+    public void RemoveChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
 
+    public void UpdateGames(RoomInfo[] rooms)
+    {
+    }
+    
+   
 
     public void ShowGames(RoomInfo[] rooms)
     {
+        RemoveChildren();
         int itemCount = rooms.Length;
-        RectTransform rowRectTransform = itemPrefab.GetComponent<RectTransform>();
-        RectTransform containerRectTransform = gameObject.GetComponent<RectTransform>();
+        
 
         //calculate the width and height of each child item.
-        float width = containerRectTransform.rect.width / columnCount;
-        float ratio = width / rowRectTransform.rect.width;
+        float containerWidth = containerRectTransform.rect.width;
+        float rowWidth = rowRectTransform.rect.width;
+        float ratio = containerWidth / rowWidth;
         float height = rowRectTransform.rect.height * ratio;
-        int rowCount = itemCount / columnCount;
-        if (itemCount % rowCount > 0)
-            rowCount++;
 
+        
         //adjust the height of the container so that it will just barely fit all its children
-        float scrollHeight = height * rowCount;
+        float scrollHeight = height * rooms.Length;
         containerRectTransform.offsetMin = new Vector2(containerRectTransform.offsetMin.x, -scrollHeight / 2);
         containerRectTransform.offsetMax = new Vector2(containerRectTransform.offsetMax.x, scrollHeight / 2);
         int i = 0;
@@ -46,11 +62,11 @@ public class ScrollableList : MonoBehaviour
             //move and size the new item
             RectTransform rectTransform = newItem.GetComponent<RectTransform>();
 
-            float x = -containerRectTransform.rect.width / 2 + width * (i % columnCount);
+            float x = -containerRectTransform.rect.width / 2 + containerWidth * (i % columnCount);
             float y = containerRectTransform.rect.height / 2;
             rectTransform.offsetMin = new Vector2(x, y);
 
-            x = rectTransform.offsetMin.x + width;
+            x = rectTransform.offsetMin.x + containerWidth;
             y = rectTransform.offsetMin.y + height;
             rectTransform.offsetMax = new Vector2(x, y);
 
@@ -59,10 +75,10 @@ public class ScrollableList : MonoBehaviour
             gameWidget.GameName.text = roomInfo.name;
             gameWidget.CurrentPlayers.text = roomInfo.playerCount.ToString();
             gameWidget.JoinButton.enabled = roomInfo.playerCount < roomInfo.maxPlayers;
+
+            //Debug.Log("Game Widget created");
   
         }
-
-
     }
 
 }
