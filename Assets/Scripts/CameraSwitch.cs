ed9adce253a950157137465e2bb354384a465b3a
unknown <Jonas.Eicher@KCM-PC03.fw-c.local>
2014-11-09 18:32:33 +0100

    added 3d-ship added minigame-test added lighting added cameras edited camera controller added beer

---
+++ Assets/Scripts/CameraSwitch.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour {
+
+	public Camera camera;
+	public Camera camera2;
+
+	// Use this for initialization
+	void Start () {
+	
+		camera.enabled = true;
+		camera2.enabled = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//This will toggle the enabled state of the two cameras between true and false each time
+		if (Input.GetKeyUp(KeyCode.Space)) {
+			camera.enabled = !camera.enabled;
+			camera2.enabled = !camera2.enabled;
+
+	}
+}
+}
---
+++ Assets/Scripts/SmoothOverview.cs
@@ -0,0 +1,31 @@
+﻿// Converted from UnityScript to C# at http://www.M2H.nl/files/js_to_c.php - by Mike Hergaarden
+
+using UnityEngine;
+using System.Collections;
+
+public class SmoothOverview : MonoBehaviour
+{
+	// The target we are following
+	public Transform target;
+	// The distance in the x-z plane to the target
+	public float distance = 10.0f;
+	// the height we want the camera to be above the target
+	public float height = 10.0f;
+	// How much we 
+	public float angle;
+	// position damping
+	public float positionDamping;
+	
+	void  LateUpdate ()
+	{
+		// Early out if we don't have a target
+		if (!target)
+			return;
+		
+		// Calculate the current rotation angles
+		float wantedHeight = target.position.y + height;
+		float currentHeight = transform.position.y;
+
+		transform.position = Vector3.Lerp (transform.position, target.position+ Vector3.up*height, positionDamping*Time.time);
+	}
+}
