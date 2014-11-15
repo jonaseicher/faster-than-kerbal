32820ffd2793933d0fc6f40a3a965b5c0f5b46aa
Mjeno <julian_c@gmx.de>
2014-11-15 19:45:49 +0100

    added Unitykit plugin

---
+++ Assets/UnityKit/Code/Components/UKMonoBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UKMonoBehaviour : MonoBehaviour 
+{
+    private Transform cachedTransform;
+    public Transform CachedTransform
+    {
+        get
+        {
+            if (cachedTransform == null) cachedTransform = GetComponent<Transform>();
+            return cachedTransform;
+        }
+    }
+
+#if UNITY_4_6
+    private RectTransform cachedRectTransform;
+    public RectTransform CachedRectTransform
+    {
+        get
+        {
+            if (cachedRectTransform == null) cachedRectTransform = GetComponent<RectTransform>();
+            return cachedRectTransform;
+        }
+    }
+#endif
+}
